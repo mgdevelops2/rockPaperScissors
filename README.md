6d275c80c9bfe3e96e447c7f191ddb253196d40a
Mike Gonzalez <Mike.Gon1217@gmail.com>
2022-06-07 18:05:49 -0400

    Added a blank index.html, to hold the the js link, and run the console game.

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # rockPaperScissors
 TOP - rock paper scissors project, played against ai. 
+
+This is a project, from The Odin Project. 
+
+And, also my first js program built from scratch, incporporating everthing I've learned from previous lessons in js. 
---
+++ javascript.js
@@ -0,0 +1,8 @@
+// this function is for the computer playing. It will return a random number from 0 < 3;
+//the idea is to get options from index [0], up to Max number 3 but not including 3. 
+
+function computerPlay(){
+    let randomNum = Math.floor(Math.random()*3);
+    console.log(randomNum);
+}
+
