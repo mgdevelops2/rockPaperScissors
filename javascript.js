b7ac62dcb7a91e7d401839c19dceab21df554c8d
Mike Gonzalez <Mike.Gon1217@gmail.com>
2022-06-08 10:58:00 -0400

    I've added calculations for a playRound. Going to run some test.

--- javascript.js
+++ javascript.js
@@ -3,18 +3,36 @@
 
 function computerPlay(){
     let randomNum = Math.floor(Math.random()*3);
-    console.log("computer plays: " + randomNum);
 
-    // now we will be adding an if scenario that will return either ‘Rock’, ‘Paper’ or ‘Scissors’. 
+    // now we will be adding an if else scenario that will return either ‘Rock’, ‘Paper’ or ‘Scissors’. 
         if (randomNum === 0){
-            console.log("rock");
+            return "rock";
             }
-
             else if (randomNum === 1){
-                console.log("paper");
+                return "paper";
             }
-            
             else {
-                console.log("scissor");
+                return "scissor";
             }
         }
+
+const playerSelection = prompt("what do you choose?", "".toLowerCase());
+  const computerSelection = computerPlay();
+        
+// Now we will create a function that plays a single round of Rock Paper Scissors. 
+// The function should take two parameters - the playerSelection and computerSelection
+// and then return a string that declares the winner of the round like so: "You Lose! Paper beats Rock"
+
+  
+function playRound(playerSelection, computerSelection) {
+    // your code here!
+    if (playerSelection === computerSelection){
+        console.log("It's a tie, Bummer!");
+    } else if (playerSelection === "rock" && computerSelection === "paper" || playerSelection === "paper" && computerSelection === "scissor" || playerSelection === "scissor" && computerSelection === "rock"){
+        console.log("You LOSE! " + computerSelection + "beats " + playerSelection);
+    } else (console.log(" You win!"))
+  }
+  
+  const playerSelection = "rock";
+  const computerSelection = computerPlay();
+  console.log(playRound(playerSelection, computerSelection));
